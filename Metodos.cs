f1dbc372cbee9eb01f43b2c0ea83dbcf1c234d87
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2023-10-24 13:13:12 -0700

    Agregar archivos de proyecto.

---
+++ Metodos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALGORITMODE_ORDENAMIENTO
+{
+
+    public class Metodos
+    {
+        int[] valores = { 19, 25, 99, 100, 55, 1, 468, 0, 250, 45, 25, 68, 5 };
+        public void insertar()
+        {
+            int moverElemento;
+            int insertar;
+            for (int i = 1; i < valores.Length; i++)
+            {
+                insertar = valores[i];
+                moverElemento = i;
+                while (moverElemento > 0 && valores[moverElemento - 1] > insertar)
+                {
+                    valores[moverElemento] = valores[moverElemento - 1];
+                    moverElemento--;
+                    Console.WriteLine($"dentro del while{string.Join(" | ", valores)}");
+                }
+                valores[moverElemento] = insertar;
+                Console.WriteLine($"dentro del for {string.Join(" | ", valores)}");
+            }
+        }
+        public void Burbuja()
+        {
+            int temp;
+            for (int i = 0; i < valores.Length - 1; i++)
+            {
+                for (int j=i+1;j<valores.Length;j++)
+                {
+                    if (valores[j] < valores[i])
+                    { 
+                        temp= valores[i];
+                        valores[i] = valores[j];
+                        valores[j] = temp;
+                    }
+                }
+                Console.WriteLine($"dentro del for{string.Join(" | ", valores)}");
+            }
+            Console.WriteLine(string.Join(" | ", valores));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ALGORITMODE_ORDENAMIENTO;
+
+Console.WriteLine("Hello, World!");
+
+Metodos metos= new Metodos();
+metos.insertar();
+Console.WriteLine("=================================================================================================================");
+metos.Burbuja();
